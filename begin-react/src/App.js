f79d94c28ae0870088a3f308ff2a01cf86d0af4d
조석준 <vcxttlvcx@naver.com>
2021-06-20 17:27:59 +0900

    Add 1장 01 ~ 04 JSX

---
+++ begin-react/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem',
+  };
+
+  return (
+    <>
+      {/* 주석은 화면에 보이지 않습니다 */}
+      <Hello
+      // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+  );
+}
+
+export default App;
