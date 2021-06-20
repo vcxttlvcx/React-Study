23e447b2374ffa823a1194a1de4caab880444105
조석준 <vcxttlvcx@naver.com>
2021-06-20 17:55:27 +0900

    Update 1장 08. input 상태 관리하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Counter from './Counter';
+import React, { useState } from 'react';
+import InputSample from './InputSample';
 
 function App() {
-  return <Counter />;
+  return <InputSample />;
 }
 
 export default App;
